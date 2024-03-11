f07d6e7bbf0e7f50acbd91d9e3e068546b5bbd22
Tokisaki-Galaxy <61621971+Tokisaki-Galaxy@users.noreply.github.com>
2024-03-11 21:55:01 +0800

    更新绘制五角星的函数，添加了根据距离绘制五角星的功能

--- 2-6 绘制指定中心位置和半径的五角星.py
+++ 2-6 绘制指定中心位置和半径的五角星.py
@@ -21,22 +21,53 @@ def calc_pos(ori_x,ori_y,r):
     star_pos['y5'] = ori_y+r*m.sin(m.radians(234))
     print(star_pos)
     print("(x1,y1)和(x4,y4)的距离L为"+str(m.sqrt((star_pos['x4'] - star_pos['x1'])**2 + (star_pos['y4'] - star_pos['y1'])**2)))
+    return m.sqrt((star_pos['x4'] - star_pos['x1'])**2 + (star_pos['y4'] - star_pos['y1'])**2)
 
 def draw_fiveanglestar(ori_x,ori_y,r):
     global star_pos
-    t.setup();t.speed(1000)
+    t.Screen();t.speed(1000)
     # 先画外接圆
-    t.penup();t.goto(ori_x-ori_x/2, ori_y);t.pendown()
+    t.penup();t.goto(ori_x, ori_y-r);t.pendown()
     t.circle(r)
     # 回到中心位置，画x1，y1
     t.penup();t.goto(ori_x, ori_y);t.pendown()
+    t.color('red');t.begin_fill()
     t.goto(star_pos['x1'],star_pos['y1'])
+    t.goto(star_pos['x3'],star_pos['y3'])
+    t.goto(star_pos['x5'],star_pos['y5'])
+    t.goto(star_pos['x2'],star_pos['y2'])
+    t.goto(star_pos['x4'],star_pos['y4'])
+    t.goto(star_pos['x1'],star_pos['y1'])
+    t.end_fill()
 
+    t.penup()
+    t.goto(50,-170)
+    t.pendown()
+    t.write("2306020123 韩镒名",font=("Arial",20,"normal"))
+    t.hideturtle();
 
-    t.hideturtle();t.done()
+def draw_using_L(ori_x,ori_y,r,L):
+    # 先画外接圆
+    t.penup();t.goto(ori_x, ori_y-r);t.pendown()
+    t.circle(r)
+    # 回到中心位置
+    t.penup();t.goto(ori_x, ori_y);t.pendown()
+    t.begin_fill()
+    t.setheading(180);t.right(18)
+    t.forward(r)
+    t.setheading(0)
+    t.right(18*2)
+    t.forward(L)
+    for a in range(4):
+        t.left(180);t.right(36)
+        t.forward(L)
+    t.end_fill()
+    t.done()
+
 if __name__=='__main__':
     r=100
     ori_x=30
     ori_y=40
-    calc_pos(ori_x,ori_y,r)
+    L=calc_pos(ori_x,ori_y,r)
     draw_fiveanglestar(ori_x,ori_y,r)
+    draw_using_L(-60,-80,r,L)
